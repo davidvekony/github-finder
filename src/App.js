8184be4a4f2ba6d9f394f88e7caf9d615ff551a5
David Vekony <68352412+davidvekony@users.noreply.github.com>
2022-04-28 08:44:32 +0100

    create and inject alert component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
+import Alert from "./components/layout/Alert";
 import { GithubProvider } from "./context/github/GithubContext";
 import { AlertProvider } from "./context/alert/AlertContext";
 
@@ -16,7 +17,15 @@ function App() {
             <Navbar />
             <main className="container mx-auto px-3 pb-12">
               <Routes>
-                <Route path="/" element={<Home />} />
+                <Route
+                  path="/"
+                  element={
+                    <>
+                      <Alert />
+                      <Home />
+                    </>
+                  }
+                />
                 <Route path="/about" element={<About />} />
                 <Route path="/notfound" element={<NotFound />} />
                 <Route path="/*" element={<NotFound />} />
--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,11 +1,13 @@
 import { useState, useContext } from "react";
 import GithubContext from "../../context/github/GithubContext";
+import AlertContext from "../../context/alert/AlertContext";
 import { searchUsers } from "../../context/github/GithubActions";
 
 function UserSearch() {
   const [text, setText] = useState("");
 
   const { users, dispatch } = useContext(GithubContext);
+  const { setAlert } = useContext(AlertContext);
 
   const handleChange = (e) => setText(e.target.value);
 
@@ -13,7 +15,7 @@ function UserSearch() {
     e.preventDefault();
 
     if (text === "") {
-      alert("Please enter something");
+      setAlert("Please enter something", "error");
     } else {
       dispatch({ type: "SET_LOADING" });
       const users = await searchUsers(text);
